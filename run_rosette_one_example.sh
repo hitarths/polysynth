864a8e44fb9934d42c759e9708cce544b3dac2c3
Hitarth <32850459+hitarths@users.noreply.github.com>
2023-01-07 10:36:45 +0100

    readme v5

--- run_rosette_one_example.sh
+++ run_rosette_one_example.sh
@@ -1,3 +1,4 @@
 # Running a single rosette example
-
+echo "Running Racket on Example: benchmarks-rosette/fermat1.rkt"
+echo "Time Taken:"
 racket benchmarks-rosette/fermat1.rkt
--- run_sketch_one_example.sh
+++ run_sketch_one_example.sh
@@ -1,3 +1,3 @@
 # Running a single sketch example
 
-./external-tools/sketch-1.7.6/sketch-frontend/sketch --fe-fpencoding AS_FIXPOINT benchmarks-sketch/prod_bin.sk
+./external-tools/sketch-1.7.6/sketch-frontend/sketch --fe-fpencoding AS_FIXPOINT benchmarks-sketch/prodbin.sk
