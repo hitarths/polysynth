c78fa82dd408f93e9a0d27e3056a1b56871c01f0
Hitarth <32850459+hitarths@users.noreply.github.com>
2023-02-27 16:08:09 +0100

    unix file test

---
+++ compile_all_results.py
@@ -0,0 +1,13 @@
+import csv
+
+final_output = "compile-table.csv"
+
+sketch_csv = "sketch-outputs.csv"
+rosette_csv = "rosette-outputs.csv"
+polysynth_csv = "polysynth-outputs.csv"
+
+
+sketch_file = open(sketch_csv)
+rosette_file = open(rosette_csv)
+polysynth_file = open(polysynth_csv)
+
